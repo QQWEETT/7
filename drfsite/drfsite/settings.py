3da63fcaf57200a27a2e7965e175c9ca503f369b
QQWEETT <94145619+QQWEETT@users.noreply.github.com>
2022-03-03 13:44:03 +0300

    Update settings.py

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'drfsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'drfsite3',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
+        'NAME': 'databasename',
+        'USER': 'username',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
