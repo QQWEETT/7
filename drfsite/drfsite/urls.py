90f2918865a1c0debd6fcc66bed0487dda35575f
QQWEETT <94145619+QQWEETT@users.noreply.github.com>
2022-03-03 13:44:42 +0300

    Update urls.py

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -9,13 +9,7 @@ from product.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('product.urls')),
-    path('api/v1/cart/', CartItemView.as_view(), name="cart"),
-    path('api/v1/cart/add/', CartItemAddView.as_view()),
-    path('api/v1/cart/delete/<int:pk>/', CartItemDelView.as_view()),
-    path('api/v1/cart/add_one/<int:pk>/', CartItemAddOneView.as_view()),
-    path('api/v1/cart/reduce_one/<int:pk>/', CartItemReduceOneView.as_view()),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
-
     path('api/v1/auth', include('djoser.urls')),
     re_path(r'auth/', include('djoser.urls.authtoken')),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
