1e09d92d6e4e461713e0217d048eec55c40bbde5
QQWEETT <94145619+QQWEETT@users.noreply.github.com>
2022-03-03 13:43:00 +0300

    Update urls.py

--- drfsite/product/urls.py
+++ drfsite/product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from product.views import ProductAPIList, CategoryAPIList
+from product.views import *
 
 router = routers.DefaultRouter()
 router.register(r'product', ProductAPIList)
@@ -10,5 +10,10 @@ router.register('category', CategoryAPIList)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('cart/', CartItemView.as_view(), name="cart"),
+    path('cart/add/', CartItemAddView.as_view()),
+    path('cart/delete/<int:pk>/', CartItemDelView.as_view()),
+    path('cart/add_one/<int:pk>/', CartItemAddOneView.as_view()),
+    path('cart/reduce_one/<int:pk>/', CartItemReduceOneView.as_view()),
 
 ]
